bd4170d511cba0a8d4e22bcf3e5f49d64a9f4f6b
Marc Hofmann <mhofmann713@Gmail.com>
2021-11-10 22:58:53 +0100

    git ignore

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/lenna/CLionProjects/OS
+# Install script for directory: E:/C/Projekte/OS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/lenna/CLionProjects/OS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/C/Projekte/OS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
