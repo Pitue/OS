edd7145174f50cfdf0044e487b3ec0abb863d8ef
Marc Hofmann <mhofmann713@Gmail.com>
2021-11-10 22:13:46 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS.dir/main.c.obj"
+  "OS.exe"
+  "OS.exe.manifest"
+  "OS.lib"
+  "OS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
